241cd7ade1014a7e881386b88b77b41bb0685c5a
Bozhidar Rangelov <bojidar.rangelov@gmail.com>
2019-09-22 13:00:06 +0300

    Liskov Substitution Principle added

---
+++ LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/CEO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class CEO : BaseEmployee, IManager
+    {
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 150M;
+
+            Salary = baseAmount * rank;
+        }
+
+        public void GeneratePerformanceReview()
+        {
+            Console.WriteLine("Here users can see direct report's performance.");
+        }
+
+        public void FireSomeone()
+        {
+            Console.WriteLine("You are Fired!");
+        }
+    }
+}
---
+++ LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new CEO();
+
+            accountingVP.FirstName = "John";
+            accountingVP.LastName = "Smith";
+            accountingVP.CalculatePerHourRate(4);
+
+            IManaged employee = new Employee();
+
+            employee.FirstName = "Natasha";
+            employee.LastName = "Bears";
+            employee.AssignManager(accountingVP);
+            employee.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{employee.FirstName}'s salary is {employee.Salary}/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
